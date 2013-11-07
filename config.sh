8929d7a5a72f73056ee4e3ce2cfd00469f0059bf
Hector Zarate <hecktorzr@gmail.com>
2013-11-07 21:09:54 +0100

    Included git configuration in sh script

--- config.sh
+++ config.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-
 #	== Variables ==
 
-vim_config_file_location=~/.vimrc
+#vim_config_file_location=~/.vimrc
 
+
+## Git
+
+git config --global user.email "hecktorzr@gmail.com"
+git config --global user.name "Hector Zarate"
+git config --global push.default simple
 
 #	=== Configuring Vim ===
 
-echo "Configuring Vim\n"
-rm $vim_config_file_location -f
-cp  vim/.vimrc $vim_config_file_location
-if [ -f $vim_config_file_location ];
-then 
-	echo "Success!"
-else
-	echo "Error!"
-fi
+#echo "Configuring Vim\n"
+#rm $vim_config_file_location -f
+#cp  vim/.vimrc $vim_config_file_location
+#if [ -f $vim_config_file_location ];
+#then 
+#	echo "Success!"
+#else
+#	echo "Error!"
+#fi
