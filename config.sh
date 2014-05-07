502abffe0539eeec4492cf90e4378ee7df927b90
Hector Zarate <hecktorzr@gmail.com>
2014-05-07 11:11:11 +0200

    Output format

--- config.sh
+++ config.sh
@@ -7,10 +7,15 @@ vim_config_file_location=~/.vimrc
 
 ## Git
 
+echo "Configuring Git...\n"
+
+
 git config --global user.email "hecktorzr@gmail.com"
 git config --global user.name "Hector Zarate"
 git config --global push.default simple
 git config --global color.ui true
+
+echo "\tSuccess!\n"
 
 #	=== Configuring Vim ===
 
@@ -18,8 +23,8 @@ echo "Configuring Vim...\n"
 cp  vim/.vimrc $vim_config_file_location
 if [ -f $vim_config_file_location ];
 then
-	echo "Success!"
+	echo "\tSuccess!"
 else
-	echo "Error!"
+	echo "\tError!"
 fi
 
