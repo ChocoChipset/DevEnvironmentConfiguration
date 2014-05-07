845793d58e00494e4fabc8952ee45212af2e44c5
Hector Zarate <hecktorzr@gmail.com>
2014-05-07 11:33:52 +0200

    Included Xcode theme config

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 #	== Variables ==
 
 vim_config_file_location=~/.vimrc
-
+xcode_themes_directory=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
 ## Git
 
@@ -21,6 +21,7 @@ echo "\tSuccess!\n"
 
 echo "Configuring Vim...\n"
 cp  vim/.vimrc $vim_config_file_location
+
 if [ -f $vim_config_file_location ];
 then
 	echo "\tSuccess!"
@@ -28,3 +29,17 @@ else
 	echo "\tError!"
 fi
 
+#	=== Configuring Xcode Theme ===
+
+echo "Configuring Xcode Theme...\n"
+
+mkdir $xcode_themes_directory
+cp xcode/Solarized-Dark.dvtcolortheme $xcode_themes_directory
+chmod +x $xcode_themes_directory/Solarized-Dark.dvtcolortheme
+
+if [ -f $xcode_themes_directory/Solarized-Dark.dvtcolortheme ];
+then
+echo "\tSuccess!"
+else
+echo "\tError!"
+fi
